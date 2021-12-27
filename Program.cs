07ffa039e7a52d784ef8afed47a3372b252d06c9
ESMERALDOJr <esmeraldo.log@hotmail.com>
2021-12-27 13:50:28 -0300

    Inicio CSharp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DigitalInnovationOne{
+    class Program{
+        static void Main(string[] args){
+            int numeroDeVezes = 6;
+
+            for(int i=0; i < numeroDeVezes; i++){
+                Console.WriteLine($"Enzo {i}");
+            }
+        }
+    }
+}
